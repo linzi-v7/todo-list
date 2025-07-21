716651afcf106adf2ad0fad9ef243d7ddf806261
linzi-v7 <111440224+linzi-v7@users.noreply.github.com>
2025-07-21 09:29:13 +0300

    feat: add eventBus class for pub/sub pattern

---
+++ src/modules/util/eventBus.js
@@ -0,0 +1,34 @@
+class EventBus {
+  events;
+
+  constructor() {
+    this.events = {};
+  }
+
+  subscribe(event, callback) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(callback);
+  }
+
+  unsubscribe(event, callback) {
+    if (!this.events[event]) return;
+
+    this.events[event] = this.events[event].filter((cb) => cb !== callback);
+  }
+
+  publish(event, data) {
+    if (!this.events[event]) return;
+
+    this.events[event].forEach((callback) => {
+      try {
+        callback(data);
+      } catch (error) {
+        console.error(`Error in event handler for ${event}:`, error);
+      }
+    });
+  }
+}
+
+export const eventBus = new EventBus();
